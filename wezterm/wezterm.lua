58cfefb8b69120ee55a445497616e77b62da6238
martin <1436475090@qq.com>
2026-03-13 22:53:55 +0800

    美化wezterm终端，使其更加美观

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,21 +8,28 @@ local config = {
 		bottom = 0,
 	},
 	-- 终端高度
-	initial_rows = 48,
+	initial_rows = 44,
 	-- 终端宽度
-	initial_cols = 180,
+	initial_cols = 175,
 	-- 主题
 	color_scheme = "Catppuccin Macchiato",
 	-- 字体
-	font = wezterm.font("Google Sans Code"),
+	-- font = wezterm.font("Google Sans Code"),
+	font = wezterm.font("JetBrains Mono"),
 	-- 字体大小
-	font_size = 13,
+	font_size = 13.5,
+	-- 列间距
+	cell_width = 1.05,
+	-- 行间距
+	line_height = 1.1,
 	-- 禁用标签栏
 	enable_tab_bar = true,
 	-- 禁用系统样式
 	use_fancy_tab_bar = false,
 	-- 底部标签栏
 	tab_bar_at_bottom = true,
+	-- 只有一个标签栏时隐藏标签栏
+	hide_tab_bar_if_only_one_tab = true,
 }
 
 return config
