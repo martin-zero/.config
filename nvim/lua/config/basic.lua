aa563916c5571e12c3f0e8ccdd85c89d32a322c5
martin <1436475090@qq.com>
2026-05-09 16:25:57 +0800

    添加格式化插件

--- nvim/lua/config/basic.lua
+++ nvim/lua/config/basic.lua
@@ -1,7 +1,8 @@
-vim.opt.number = true			-- 显示行号
-vim.opt.relativenumber = true		-- 相对行号
-vim.opt.cursorline = true               -- 高亮光标所在行
-vim.opt.shiftwidth = 4                  -- 缩进宽度
-vim.opt.wrap = false                            -- 不自动换行
+vim.opt.number = true -- 显示行号
+vim.opt.relativenumber = true -- 相对行号
+vim.opt.cursorline = true -- 高亮光标所在行
+vim.opt.shiftwidth = 4 -- 缩进宽度
+vim.opt.tabstop = 4 -- tab显示宽度
+vim.opt.wrap = false -- 不自动换行
 
-vim.opt.clipboard = "unnamedplus"	-- 使用系统剪切板
+vim.opt.clipboard = "unnamedplus" -- 使用系统剪切板
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,13 +1,17 @@
-vim.pack.add{
-    -- lsp 配置插件
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-    -- lsp server管理
-  { src = 'https://github.com/williamboman/mason.nvim' },
-    -- 状态栏
-  { src = 'https://github.com/nvim-lualine/lualine.nvim'},
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons'},
-}
+vim.pack.add({
+	-- lsp 配置插件
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	-- lsp server管理
+	{ src = "https://github.com/williamboman/mason.nvim" },
+	-- 格式化插件
+	{ src = "https://github.com/stevearc/conform.nvim" },
+
+	-- 状态栏
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
 
 require("mason").setup()
 require("plugins/lspconfig")
+require("plugins/conform")
 -- require("plugins/lualine")
