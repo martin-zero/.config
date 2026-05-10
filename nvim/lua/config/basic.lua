0ba31e63f7a5c1377c0d4f436edc3891c4d98562
martin <1436475090@qq.com>
2026-05-10 23:23:46 +0800

    优化基础设置排版

--- nvim/lua/config/basic.lua
+++ nvim/lua/config/basic.lua
@@ -1,10 +1,18 @@
-vim.opt.number = true -- 显示行号
-vim.opt.relativenumber = true -- 相对行号
-vim.opt.cursorline = true -- 高亮光标所在行
-vim.opt.shiftwidth = 4 -- 缩进宽度
-vim.opt.tabstop = 4 -- tab显示宽度
-vim.opt.wrap = false -- 不自动换行
-
-vim.opt.clipboard = "unnamedplus" -- 使用系统剪切板
-
-vim.cmd.colorscheme("catppuccin") -- 设置主题
+-- 显示行号
+vim.opt.number = true
+-- 相对行号
+vim.opt.relativenumber = true
+-- 高亮光标所在行
+vim.opt.cursorline = true
+-- 缩进宽度
+vim.opt.shiftwidth = 4
+-- tab显示宽度
+vim.opt.tabstop = 4
+-- 不自动换行
+vim.opt.wrap = false
+-- 窗口边框
+vim.opt.winborder = "rounded"
+-- 使用系统剪切板
+vim.opt.clipboard = "unnamedplus"
+-- 设置主题
+vim.cmd.colorscheme("catppuccin")
