bc68e2a0a0cc19fb5b4e89cfe766c52b3a749159
martin <1436475090@qq.com>
2026-05-10 19:07:50 +0800

    添加若干插件

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,6 +7,7 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	-- 状态栏
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	-- 文件图标
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 	-- 成对符号
 	{ src = "https://github.com/windwp/nvim-autopairs" },
@@ -14,6 +15,16 @@ vim.pack.add({
 	{ src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x") },
 	-- catppuccin 主题
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	-- fzf模糊搜索
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	-- 高亮todo等注释
+	{ src = "https://github.com/folke/todo-comments.nvim" },
+	-- 快速跳转
+	{ src = "https://github.com/folke/flash.nvim" },
+	-- git标识
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	-- lazygit
+	{ src = "https://github.com/kdheepak/lazygit.nvim" },
 })
 
 require("plugins.mason")
@@ -22,3 +33,7 @@ require("plugins.lualine")
 require("plugins.nvim-autopairs")
 require("plugins.nvim-surround")
 require("plugins.theme")
+require("plugins.fzf-lua")
+require("plugins.todo-comments")
+require("plugins.flash")
+require("plugins.lazygit")
