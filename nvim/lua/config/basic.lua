e418fb00b217894103ad428a254ee0512bd0ac28
martin <1436475090@qq.com>
2026-05-11 13:30:02 +0800

    完善基础配置

--- nvim/lua/config/basic.lua
+++ nvim/lua/config/basic.lua
@@ -14,5 +14,12 @@ vim.opt.wrap = false
 vim.opt.winborder = "rounded"
 -- 使用系统剪切板
 vim.opt.clipboard = "unnamedplus"
+-- 搜索忽略大小写
+vim.opt.ignorecase = true
+-- 保存修改历史(退出重进还可以撤销上一步)
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.local/state/nvim/undo")
 -- 设置主题
 vim.cmd.colorscheme("catppuccin")
+-- 上下保留5行缓冲区
+vim.opt.scrolloff = 5
