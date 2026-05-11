4436290b078712b8ae25b0116f26fb88f1c6c0dc
martin <1436475090@qq.com>
2026-05-11 13:29:37 +0800

    补全根init.lue注释

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
-require('vim._core.ui2').enable({})
-
-require('plugins')
-require('config/basic')
-
+-- 开启ui2
+require("vim._core.ui2").enable({})
+-- 插件配置
+require("plugins")
+-- 基础配置
+require("config.basic")
+-- 快捷键配置
+require("config.keymaps")
