1d581b9cc35bd69f202d3092efe6bf61a3624465
martin <1436475090@qq.com>
2026-05-28 13:27:13 +0800

    添加代码大纲aerial插件

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -109,4 +109,28 @@ return {
       end)
     end,
   },
+
+  --代码大纲
+  {
+    "stevearc/aerial.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    opts = {
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    },
+
+    keys = {
+      {
+        "<leader>a",
+        "<cmd>AerialToggle!<CR>",
+        desc = "Toggle Aerial",
+      },
+    },
+  },
 }
